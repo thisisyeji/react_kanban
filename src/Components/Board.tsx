eccdd229a3b5da55a67cae57da61357feeb8ba85
yjkim0109 <yjkkim0109@gmail.com>
2022-10-06 15:33:31 +0900

    Feat: Board 3개 만들기

---
+++ src/Components/Board.tsx
@@ -0,0 +1,46 @@
+import { Droppable } from 'react-beautiful-dnd';
+import DragabbleCard from './DragabbleCard';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+	width: 300px;
+	padding: 20px 10px;
+	padding-top: 10px;
+	background-color: ${(props) => props.theme.boardColor};
+	border-radius: 5px;
+	min-height: 300px;
+`;
+
+const Title = styled.h2`
+	text-align: center;
+	font-weight: 600;
+	margin-bottom: 10px;
+	font-size: 18px;
+`;
+
+interface IBoardProps {
+	toDos: string[];
+	boardId: string;
+}
+
+function Board({ toDos, boardId }: IBoardProps) {
+	return (
+		<Wrapper>
+			<Title>{boardId}</Title>
+			<Droppable droppableId={boardId}>
+				{(magic) => (
+					<div ref={magic.innerRef} {...magic.droppableProps}>
+						{toDos.map((toDo, index) => (
+							<DragabbleCard key={toDo} index={index} toDo={toDo} />
+						))}
+
+						{/* placeholder: 드래그 하는 동안 영역을 고정시킴 */}
+						{magic.placeholder}
+					</div>
+				)}
+			</Droppable>
+		</Wrapper>
+	);
+}
+
+export default Board;
