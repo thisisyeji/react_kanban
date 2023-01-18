9bc43402912e0e43eb3aaa8518a5515696250202
yjkim0109 <yjkkim0109@gmail.com>
2023-01-18 22:00:06 +0900

    feat: Add recoil-persist for localStorage

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,4 +1,5 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
 export interface ITodo {
 	id: number;
@@ -9,6 +10,27 @@ interface IToDoState {
 	[key: string]: ITodo[];
 }
 
+/*
+export const localStorageEffect =
+	(key: string) =>
+	({ setSelf, onSet }: any) => {
+		const savedValue = localStorage.getItem(key);
+		// setSelf -> Callbacks to set or reset the value of the atom.
+		if (savedValue != null) {
+			setSelf(JSON.parse(savedValue));
+		}
+
+		// onSet -> Subscribe to changes in the atom value.
+		onSet((newValue: any, _: any, isReset: boolean) => {
+			isReset
+				? localStorage.removeItem(key)
+				: localStorage.setItem(key, JSON.stringify(newValue));
+		});
+	};
+	*/
+
+const { persistAtom } = recoilPersist();
+
 export const toDoState = atom<IToDoState>({
 	key: 'toDo',
 	default: {
@@ -16,4 +38,5 @@ export const toDoState = atom<IToDoState>({
 		Doing: [],
 		Done: [],
 	},
+	effects_UNSTABLE: [persistAtom],
 });
