ae6cd053aa0f75022661335d94cb74b9f082148d
yjkim0109 <yjkkim0109@gmail.com>
2023-01-18 22:03:53 +0900

    refactor : Use arrow function

--- src/Components/DragabbleCard.tsx
+++ src/Components/DragabbleCard.tsx
@@ -4,12 +4,12 @@ import styled from 'styled-components';
 
 const Card = styled.div<{ isDragging: boolean }>`
 	border-radius: 5px;
-	padding: 10px 10px;
-	margin-bottom: 5px;
 	background-color: ${(props) =>
-		props.isDragging ? '#e4f2ff' : props.theme.cardColor};
+		props.isDragging ? '#74b9ff' : props.theme.cardColor};
 	box-shadow: ${(props) =>
-		props.isDragging ? '0px 2px 5px rgba(0, 0, 0, 0.05)' : 'none'};
+		props.isDragging ? '0px 2px 15px rgba(0,0,0,0.05)' : 'none'};
+	padding: 10px;
+	margin-bottom: 5px;
 `;
 
 interface IDraggableCardProps {
@@ -18,21 +18,20 @@ interface IDraggableCardProps {
 	index: number;
 }
 
-function DragabbleCard({ toDoId, toDoText, index }: IDraggableCardProps) {
+const DragabbleCard = ({ toDoId, toDoText, index }: IDraggableCardProps) => {
 	return (
-		// dnd에서는 key와 draggableId는 같아야함
 		<Draggable draggableId={toDoId + ''} index={index}>
 			{(magic, snapshot) => (
 				<Card
 					isDragging={snapshot.isDragging}
 					ref={magic.innerRef}
-					{...magic.dragHandleProps}
-					{...magic.draggableProps}>
+					{...magic.draggableProps}
+					{...magic.dragHandleProps}>
 					{toDoText}
 				</Card>
 			)}
 		</Draggable>
 	);
-}
+};
 
 export default React.memo(DragabbleCard);
