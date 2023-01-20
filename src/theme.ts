47b8244f4730cdabe3f86d29d0abd6a23e0074f6
yjkim0109 <yjkkim0109@gmail.com>
2023-01-20 17:22:58 +0900

    feat: Modify theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from 'styled-components';
 
 export const darkTheme: DefaultTheme = {
-	bgColor: '#3F8CF2',
-	boardColor: '#DADFE9',
+	bgColor: '#449980',
+	boardColor: '#efefef',
 	cardColor: 'white',
 };
